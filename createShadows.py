9e58e2c518932e58105fe9b3e1e9ecd1a94b03bd
Madis Kaspar Nigol <madis1995@gmail.com>
2022-04-25 12:06:33 +0300

    Commented out mesh

--- createShadows.py
+++ createShadows.py
@@ -2,15 +2,16 @@ import cloudComPy as cc
 import cloudComPy.PCV
 import numpy as np
 import open3d as o3d
-
-cloud = cc.loadPointCloud("/home/madis/Downloads/tykk.las")           
+print("Started")
+cloud = cc.loadPointCloud("moon_DS.ply")           
 print("cloud name: %s"%cloud.getName())
 
-filteredCloud = cc.CloudSamplingTools.noiseFilter(cloud, 1,1,removeIsolatedPoints=True).getAssociatedCloud()
-
-mesh = cc.ccMesh.triangulate(filteredCloud,cc.TRIANGULATION_TYPES.DELAUNAY_2D_AXIS_ALIGNED,updateNormals=True)
-
+# cloud = cc.CloudSamplingTools.noiseFilter(cloud, 1,1,removeIsolatedPoints=True).getAssociatedCloud()
+# print("Filtered")
+mesh = cc.ccMesh.triangulate(cloud,cc.TRIANGULATION_TYPES.DELAUNAY_2D_AXIS_ALIGNED,updateNormals=True)
+print("Created mesh")
 mesh.laplacianSmooth()
+print("Smoothened")
 
 # cc.SaveMesh(mesh, "mesh.ply")
 
@@ -22,11 +23,12 @@ transMat = cc.ccGLMatrix.FromToRotation((0,0,1), initialAngle) #must not come fr
 plane = cloudComPy.ccPlane(5,5, transMat).samplePoints(False, 2)
 
 pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(cc.ccPointCloud.toNpArray(filteredCloud))
+pcd.points = o3d.utility.Vector3dVector(cc.ccPointCloud.toNpArray(cloud))
 o3d.io.write_point_cloud("points.ply", pcd)
-
+print("Wrote out points")
 for i in range(18):
-  cc.PCV.computeShadeVIS([mesh], cloudWithNormals=plane)
-  scalfield = cc.ccPointCloud.getScalarField(filteredCloud, filteredCloud.getNumberOfScalarFields()-1)
+  cc.PCV.computeShadeVIS([cloud], cloudWithNormals=plane)
+  scalfield = cc.ccPointCloud.getScalarField(cloud, cloud.getNumberOfScalarFields()-1)
   np.save("shadows/shadows"+str(i), cc.ScalarField.toNpArray(scalfield))
   cc.ccPointCloud.applyRigidTransformation(plane, rotSmall)
+  print("Wrote ", i)
